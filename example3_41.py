f9b2176c3939a3197bf86fd0dff124915b75b644
Nicholas Tan <nicktantk@gmail.com>
2025-07-26 01:41:20 +0800

    example 3.4 2 with long-short market neutral strategy

--- example3_41.py
+++ example3_41.py
@@ -3,10 +3,15 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt 
+import yfinance as yf
 
-df = pd.read_excel('./excels/IGE.xls')
+# data = yf.download('IGE', start='2015-01-01', end='2025-06-30')
+# data.to_excel('./excels/IGE.xlsx', engine='openpyxl')
+
+
+df = pd.read_excel('./excels/IGE.xlsx')
 df.sort_values(by='Date', inplace=True)
-dailyret = df.loc[:, 'Adj Close'].pct_change() # Daily returns
+dailyret = df.loc[:, 'Close'].pct_change() # Daily returns
 excessRet = dailyret - 0.04/252 # Excess returns = Strategy returns - risk free rate (financing cost)
 sharpeRatio = np.sqrt(252) * np.mean(excessRet) / np.std(excessRet)
 print(sharpeRatio)
